90ebc96926fd2efac9ddf828b42429879b9da295
Liff <liiffigc4@gmail.com>
2026-08-10 09:36:04 +0800

    updt

--- To-Do-List-Python/python-todo-list.py
+++ To-Do-List-Python/python-todo-list.py
@@ -1,44 +1,97 @@
 import os
 
-task_list = []
+class Task:
+    def __init__(self):
+        self.__task_list = []
 
-def add_list(name):
-    global task_list
-    task_list.append(name)
+    def add_task(self, name):
+        self.__task_list.append(
+            {"name": name, "status": False}
+        )
 
-def remove_list():
-    pass
+    def remove_task(self, no):
+        self.__task_list.pop(no-1)
 
-def show_menu():
-    pass
+    def get_task(self):
+        return self.__task_list
+
+    def complete_task(self, no):
+        self.__task_list[no-1]["status"] = True
+
+    def show_menu(self):
+        print("\n==========TODO LIST==========\n")
+        print("[1] View Task")
+        print("[2] Add Task")
+        print("[3] Remove Task")
+        print("[4] Complete Task")
+        print("[0] Exit")
+        print("\n-----------------------------")
+
+
+my_task = Task()
 
 while True:
-    print("\n==========TODO LIST==========\n")
-    print("[1] Lihat Task")
-    print("[2] Tambah Task")
-    print("[3] Hapus Task")
-    print("[0] Keluar")
-    print("\n-----------------------------")
+    my_task.show_menu()
 
-    choice = int(input("\n>> Pilihan : "))
+    choice = int(input("\n>> Choice : "))
 
     match choice:
         case 1:
             print()
-            for i, task in enumerate(task_list, start=1):  
-                print("{i}. {task}")
+            for i, task in enumerate(my_task.get_task(), start=1):
+                if task["status"]:
+                    status = "✅"
 
-            input("\nTekan Enter untuk lanjut...")
+                else:
+                    status = "❌"
+
+                print(f"{i}. {task["name"]} {status}")
+
+            input("\nPress Enter to continue...")
 
         case 2:
-            name_list = input("Masukkan nama list : ")
-            add_list(name_list)            
+            total = int(input("\nEnter the number of tasks to add: ")) # ✅ ❌
+
+            for i in range(total):
+                name = input(f"Task {i+1} : ")
+                my_task.add_task(name)            
 
         case 3:
-            pass
+            number = int(input("\nEnter task number to remove: "))
+            my_task.remove_task(number)
 
+        case 4:
+            status = True
+
+            while status:
+                print()
+                for i, task in enumerate(my_task.get_task(), start=1):
+                    if task["status"]:
+                        status = "✅"
+                
+                    else:
+                        status = "❌"
+                
+                    print(f"{i}. {task["name"]} {status}")
+                
+                number = int(input("\nEnter the task number to complete: "))
+                my_task.complete_task(number)
+
+                while True:
+                    exit = input("Any other tasks to complete? (y/n): ").lower()
+
+                    if len(exit) == 1 and exit in ("y", "n"):
+                        if exit == "y":
+                            break
+
+                        else:
+                            status = False
+                            break
+
+                    print("Please enter only 'y' or 'n'.")
+            
         case 0:
+            print("\nGood Bye!")
             break
-            pass
 
-    os.system("clear")
+    os.system("cls")
